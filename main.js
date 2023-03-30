0542b45119bf7d72eb482ea7d5017e46f06470d2
Hansae <byu0324@gmail.com>
2023-03-30 18:59:47 +0900

    modefy complete

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const menu = document.querySelector('.menu');
-const visible = document.querySelector('.top ul');
+const visible = document.querySelector('.top .mobile');
+
+visible.style.opacity = '0';
 
 function visibility () {
-  if(visible.style.display == 'none') {
-    visible.style.display = 'flex';
+  if(visible.style.opacity == '0') {
+    visible.style.opacity = '01';
     menu.style.opacity = '.5';
   } else {
-    visible.style.display = 'none';
+    visible.style.opacity = '0';
     menu.style.opacity = '1';
   }
 }
